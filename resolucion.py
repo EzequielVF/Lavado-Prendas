f07e7674950fff227af2f9a6241eae15e28748df
Ezequiel <evilardo@fi.uba.ar>
2024-04-07 20:50:59 -0300

    Defino estructuras iniciales

--- resolucion.py
+++ resolucion.py
@@ -1,8 +1,20 @@
 import time
 
+class Lavanderia:
+	def __init__(self):
+		self.nroLavadoActual = 0
+		self.prendas = []
+
+class Prenda:
+	def __init__(self,nro):
+		self.nro = nro
+		self.tiempo = 0
+		self.incompatibilidades = {}
+		self.lavada = False
+
 def main():
 	inicio = time.time()
- 
+	
  
 	fin = time.time()
 	print(fin-inicio)
