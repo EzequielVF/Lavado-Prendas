9be034deabc5a433dc040f9dd61e9807426c4715
Ezequiel <evilardo@fi.uba.ar>
2024-05-05 15:26:02 -0300

    Coloreo de grafos

--- resolucion.py
+++ resolucion.py
@@ -2,11 +2,8 @@ import time
 
 class Lavanderia:
 	def __init__(self):
-		self.nroLavadoActual = 0
 		self.prendas = []
-		self.cola = []
 		self.cantidad_prendas = 0
-		self.prendas_lavadas = 0
 	
 	def cargar_prendas(self, nro):
 		self.cantidad_prendas = nro
@@ -24,9 +21,8 @@ class Lavanderia:
 		self.prendas[prenda-1].cargar_tiempo(tiempo)
  
 	def lavar(self):
-		self.prendas.sort()
 		archivo = open("respuestasTP2.txt","w")
-		i = 0
+		self.prendas.sort()
 		lavado = 1
 		colores = [-1] * self.cantidad_prendas
 		for i in range(self.cantidad_prendas):
@@ -34,55 +30,29 @@ class Lavanderia:
 				colores[i] =  lavado
 				self.prendas[i].lavado = lavado
 				for j in range(self.cantidad_prendas):
-					if colores[j] == -1 and self.prendas[i].es_compatible_con(self.prendas[j].nro) == True:
+					if j == i:
+						continue
+					if colores[j] == -1 and self.lavado_compatible(j, lavado, colores):
 						colores[j] =  lavado
 						self.prendas[j].lavado = lavado
 				lavado += 1
-
 		prendas_ordenadas = sorted(self.prendas, key=lambda x: x.lavado)
 		for prenda in prendas_ordenadas:
 			archivo.write(str(prenda.nro) + " " + str(prenda.lavado) + "\n")
-		#while(self.prendas_lavadas <= self.cantidad_prendas - 1):
-		#	if (self.prendas[i].prenda_lavada() == False) and (self.cola_incompatible(self.prendas[i].nro) == False):
-		#		self.cola.append(self.prendas[i])
-		#	i = i + 1
-		#	if i == self.cantidad_prendas:
-		#		for j in self.cola:
-		#			j.lavar_prenda()
-		#			self.prendas_lavadas = self.prendas_lavadas + 1
-		#			archivo.write(str(j.nro) + " " + str(lavado) + "\n")
-		#		self.cola = []
-		#		lavado = lavado + 1
-		#		i = 0
 		archivo.close()
 	
-	def lavado_compatible(self, i, j):
-		for x in range(i):
-			if self.prendas[x].es_compatible_con(self.prendas[j].nro) == False:
+	def lavado_compatible(self, j, lavado_actual, colores):
+		for x in range(j):
+			if self.prendas[x].es_compatible_con(self.prendas[j].nro) == False and colores[x] == lavado_actual:
 				return False
-		return True
- 		
-	def cola_incompatible(self, nro):
-		if len(self.cola) == 0:
-			return False
-		flag = False
-		for i in self.cola:
-			aux = i.es_compatible_con(nro)
-			if aux == False:
-				flag = True
-				break
-		return flag
+		return True		
  	
 class Prenda:
 	def __init__(self,nro):
 		self.nro = nro
 		self.tiempo = 0
 		self.incompatibilidades = {}
-		self.lavada = False
 		self.lavado = 0
-	
-	def devolver_lista_incomp(self):
-		return self.incompatibilidades
  
 	def cargar_incompatibilidad(self, nro):
 		self.incompatibilidades.setdefault(nro, None)		
@@ -96,12 +66,6 @@ class Prenda:
 		if nro in self.incompatibilidades:
 			return False
 		return True
-  
-	def prenda_lavada(self):
-		return self.lavada
-
-	def lavar_prenda(self):
-		self.lavada = True
 
 	def __lt__(self, other):
 		return len(self.incompatibilidades) > len(other.incompatibilidades)
